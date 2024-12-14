6a83ea0b8f00e2c499748380645092d45df9b9a0
Deepak Verma <deepak.verma@live.in>
2024-12-14 20:54:45 +0530

    [Frontend] added layout header, footer and bootstrap

--- cms-frontend/src/app/app.module.ts
+++ cms-frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainContentComponent } from './layout/main-content/main-content.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    MainContentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
